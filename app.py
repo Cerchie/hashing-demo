5e8f906448bd61e237794763bc9cb07f786fb204
Lucia Cerchie <luciacerchie@gmail.com>
2020-09-03 10:54:39 -0700

    trying to solve deploy bug

--- app.py
+++ app.py
@@ -17,6 +17,48 @@ print('**********************')
 print('**********************')
 print(app.config['SECRET_KEY'])
 
+
+from sqlalchemy import exc
+from sqlalchemy import event
+from sqlalchemy import select
+
+some_engine = create_engine(...)
+
+@event.listens_for(some_engine, "engine_connect")
+def ping_connection(connection, branch):
+    if branch:
+        # "branch" refers to a sub-connection of a connection,
+        # we don't want to bother pinging on these.
+        return
+
+    # turn off "close with result".  This flag is only used with
+    # "connectionless" execution, otherwise will be False in any case
+    save_should_close_with_result = connection.should_close_with_result
+    connection.should_close_with_result = False
+
+    try:
+        # run a SELECT 1.   use a core select() so that
+        # the SELECT of a scalar value without a table is
+        # appropriately formatted for the backend
+        connection.scalar(select([1]))
+    except exc.DBAPIError as err:
+        # catch SQLAlchemy's DBAPIError, which is a wrapper
+        # for the DBAPI's exception.  It includes a .connection_invalidated
+        # attribute which specifies if this connection is a "disconnect"
+        # condition, which is based on inspection of the original exception
+        # by the dialect in use.
+        if err.connection_invalidated:
+            # run the same SELECT again - the connection will re-validate
+            # itself and establish a new connection.  The disconnect detection
+            # here also causes the whole connection pool to be invalidated
+            # so that all stale connections are discarded.
+            connection.scalar(select([1]))
+        else:
+            raise
+    finally:
+        # restore "close with result"
+        connection.should_close_with_result = save_should_close_with_result
+        
 connect_db(app)
 db.create_all()
 
